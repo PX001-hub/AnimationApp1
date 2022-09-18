e9f9c88a11e8d540f3c3bc9e252c91f8df2f52cd
Pttr <draivv@yandex.ru>
2022-09-18 23:56:50 +0300

    Initial Commit

---
+++ AnimationApp/Extention + Button.swift
@@ -0,0 +1,11 @@
+//
+//  Extention + Button.swift
+//  AnimationApp
+//
+//  Created by P Z on 15/09/2022.
+//  Copyright © 2022 Alexey Efimov. All rights reserved.
+//
+
+import Foundation
+
+
---
+++ AnimationApp/ViewController.swift
@@ -0,0 +1,30 @@
+//
+//  ViewController.swift
+//  AnimationApp
+//
+//  Created by Alexey Efimov on 02.03.2020.
+//  Copyright © 2020 Alexey Efimov. All rights reserved.
+//
+
+import Spring
+
+class ViewController: UIViewController {
+    
+    @IBOutlet var coreAnimationOrangeView: UIView!
+    
+    @IBOutlet var coreAnimationRedView: UIView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+    
+    }
+
+    @IBAction func runCoreAnimationOrange(_ sender: UIButton) {
+    }
+    
+    
+    @IBAction func runCoreAnimationRed(_ sender: UIButton) {
+    }
+    
+}
+
